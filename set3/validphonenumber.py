97dbcfa63e8bd3fccf898f0aa3fd98a6c7eaf9ff
dandumitriu33 <dandumitriu33@gmail.com>
2019-11-24 10:13:30 +0200

    Add refactored valid phone number kata

--- set3/validphonenumber.py
+++ set3/validphonenumber.py
@@ -2,13 +2,9 @@ import re
 
 
 def is_valid_phone_number(string):
-    PHONE_NUMBER_LENGTH = 14
-    if len(string) != 14:
-        return False
-    if re.search(r"[(][0-9][0-9][0-9][)][ ][0-9][0-9][0-9][-][0-9][0-9][0-9][0-9]", string):
+    if re.match(r"[(][0-9][0-9][0-9][)][ ][0-9][0-9][0-9][-][0-9][0-9][0-9][0-9]", string):
         return True
-    else:
-        return False
+    return False
 
 
 print(is_valid_phone_number("(123) 456-7890"))
